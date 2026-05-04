a8bbda20718fdba94c3dd3622ff6bf9c0ac68f68
Mangal98057 <smangal621@gmail.com>
2026-05-04 20:48:15 +0530

    variable

---
+++ Varible.js
@@ -0,0 +1,9 @@
+const name="mangal singh"//Block scoped.Cannot be reassigned
+let rollNum=23159//Block scoped.Can be reassigned
+
+//name="seema"
+console.log(name)//Assignment to constant variable.
+//console.log(rollNum)
+
+//rollNum=23160
+console.log(rollNum)
