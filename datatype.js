22aabc6557cef4b1c17fc6a3efaae308c5053458
Mangal98057 <smangal621@gmail.com>
2026-05-04 21:19:50 +0530

    datatype

---
+++ datatype.js
@@ -0,0 +1,3 @@
+"use strict"; //javascript code new version use only
+alert("hello")// we are using node js,not browsers
+
--- variable.js
+++ variable.js
@@ -1,9 +1,9 @@
 const name="mangal singh"//Block scoped.Cannot be reassigned
 let rollNum=23159//Block scoped.Can be reassigned
 
-//name="seema"
+//name="seema"// not allowed 
 console.log(name)//Assignment to constant variable.
 //console.log(rollNum)
 
-//rollNum=23160
+//rollNum=23160  //allowed 
 console.log(rollNum)
