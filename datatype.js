1bfdef87f454f610b2008ad85be27045cc73d34f
Mangal98057 <smangal621@gmail.com>
2026-05-05 07:16:31 +0530

    datatype all

--- datatype.js
+++ datatype.js
@@ -1,3 +1,58 @@
 "use strict"; //javascript code new version use only
-alert("hello")// we are using node js,not browsers
+//alert("hello")// we are using node js,not browsers
+
+//data types in javascript
+/*data types define what kind of value of variale can stroe.*/
+
+//primitive datatypes (stored by value)
+
+let name="mangal"//string
+console.log(typeof name)
+//string can use "" ,'',``(template literals)
+
+let age =21 //number
+console.log(typeof age)
+
+//for very large numbers use BigInt 
+let big = 11132334555335533n
+console.log(typeof big)
+
+//boolean (true/false)
+ let useLogin =true
+ console.log(typeof useLogin)
+
+ // underfined declared but not assigned
+ let x;
+ console.log(x)
+
+ //Null intentional empty value
+ let data=null;
+ console.log(data)
+
+ //undefined -> value not assigned 
+ //null -> value intentionally empty
+
+ //symbol unique identifier 
+ let id =Symbol("id")
+ console.log(id)
+
+
+ //Non Premitive (reference type)
+ //object stores multiple values
+ let user={
+    name:"mangal singh",
+    age: 21
+ }
+ console.log(user)
+
+ //Array (special object)
+ let arr= [1,2,3]
+ console.log(arr)
+
+ //Function 
+ function name(){
+    return "mangal"
+ }
+ //typeof operator
+ //typeof null === "object" ? it is a javascript bug legacy issue
 
