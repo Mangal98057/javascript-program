a57262dfd74d6aa671eb3224e2660226dc833d77
Mangal98057 <smangal621@gmail.com>
2026-05-05 08:51:29 +0530

    basic coversion

---
+++ conversion.js
@@ -0,0 +1,30 @@
+// what is conversion 
+//changing data from one type of to another
+let score ="mangal"
+let stringToNumber=Number(score)
+console.log(stringToNumber)//NaN
+console.log(typeof score)//print string
+
+//types of conversion 
+// 1. explicit conversion (manual)
+//you convert the type yourself
+
+//string conversion
+
+let num = 10;
+let numToString=String(num)//"10" string 
+//num.toString()
+console.log(typeof numToString)
+
+//Number conversion 
+let number = "123"
+let stringToNum= Number(number)
+console.log( typeof stringToNum)
+
+// Boolean conversion 
+let str="mangal"
+let stringToBool=Boolean(str)//true
+console.log(stringToBool)//true
+
+//2. implicit conversion 
+//javascript automatically converts types 
