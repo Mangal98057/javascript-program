1032d4d3049b84a2e438f5b3475f219e09da8b8d
Mangal98057 <smangal621@gmail.com>
2026-05-07 10:13:15 +0530

    operation

---
+++ operation.js
@@ -0,0 +1,83 @@
+//Operators are symbols used to perform operations on values and variables.
+
+// Types of Operators in JavaScript
+// 1.Arithmetic Operators
+// 2.Assignment Operators
+// 3.Comparison Operators
+// 4.Logical Operators
+// 5.Increment & Decrement
+// 6.String Operators
+// 7.Ternary Operator
+// 8.Type Operators
+// 9.Bitwise Operators
+
+
+
+//1. Arithmetic oprators
+
+// | Operator | Meaning        |
+// | -------- | -------------- |
+// | `+`      | Addition       |
+// | `-`      | Subtraction    |
+// | `*`      | Multiplication |
+// | `/`      | Division       |
+// | `%`      | Modulus        |
+// | `**`     | Exponent       |
+
+console.log(2**3) //2*2*2 =8
+
+// 2. Assignment Operators
+//Used to assign values.
+
+/*
+| Operator | Example  |
+| -------- | -------- |
+| `=`      | `x = 5`  |
+| `+=`     | `x += 2` |
+| `-=`     | `x -= 2` |
+| `*=`     | `x *= 2` |
+| `/=`     | `x /= 2` |
+
+
+*/
+
+let x = 10;
+
+x += 5;
+
+console.log(x); //15
+
+
+// 3. Comparison Operators
+//Used to compare values.
+
+/*
+
+| Operator | Meaning          |
+| -------- | ---------------- |
+| `==`     | Equal            |
+| `===`    | Strict Equal     |
+| `!=`     | Not Equal        |
+| `!==`    | Strict Not Equal |
+| `>`      | Greater Than     |
+| `<`      | Less Than        |
+| `>=`     | Greater Equal    |
+| `<=`     | Less Equal       |
+
+ */
+console.log(5 == "5"); // true
+
+/*
+4. Logical Operators
+
+Used with conditions.
+
+| Operator | Meaning |   |    |
+| -------- | ------- | - | -- |
+| `&&`     | AND     |   |    |
+|  `||     |         | ` | OR |
+| `!`      | NOT     |   |    |
+
+
+
+*/
