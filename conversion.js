d76d21a2f1e1ec5a338088b3f63f5851f1f4b841
Mangal98057 <smangal621@gmail.com>
2026-05-07 09:57:04 +0530

    conversion

--- conversion.js
+++ conversion.js
@@ -28,3 +28,21 @@ console.log(stringToBool)//true
 
 //2. implicit conversion 
 //javascript automatically converts types 
+//Explicit conversion means manually converting one data type into another.You control the conversion using built-in methods.
+
+//string to number with check type
+let str1 = "100";
+
+let num1 = Number(str1);
+
+console.log(num1);//100
+console.log(typeof num1);//number
+
+
+//number to string with check type
+let num3 = 50;
+
+let str3 = String(num3);
+
+console.log(str3);//"50"
+console.log(typeof str3);//string
