f4989fadc239a9c63ceb80b6b5b41aa5f9e53290
Mangal98057 <smangal621@gmail.com>
2026-05-09 22:07:45 +0530

    change code

--- arrowfunction.js
+++ arrowfunction.js
@@ -1,2 +1,11 @@
 //arrow function provide a concise way to write function expressions.
-const greet = name => 'mangal',
+const greet = name => `hello ${name}`;
+console.log(greet("mangal"))
+
+
+const add =(a,b)=>(a+b)
+console.log(add(3,4))
+
+
+//greet function refrence 
+//greet() function execution 
