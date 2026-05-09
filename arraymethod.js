fd9354b4f0c82011876b101ffe9160dd4b65fe2c
Mangal98057 <smangal621@gmail.com>
2026-05-09 20:46:20 +0530

    method

--- arraymethod.js
+++ arraymethod.js
@@ -7,8 +7,14 @@ const push=arr.push(5)
 const index=arr.indexOf(4)//3
 //const at=arr.at(1)//2
 //const slice=arr.slice(1,2,3)//2
-const map=arr.map((num)=>num*2)//[2,4,6,8,10]
+//const map=arr.map((num)=>num*2)//[2,4,6,8,10]
 const filter=arr.filter((num)=>num > 2)
+const map=arr.map((num)=>num+1)//[2,3,4,5,6] all arr add plus one
+const include=arr.includes(5)//check value true and false
+//const rev=arr.reverse()//arr reverse like [5,4,3,2,1]
+//const splice=arr.splice(1,2,"mangal")//add string arr return value 4,3
+const flat =arr.flat()
+const find=arr.find((num)=>num ===2)
 
 console.log(arr)
 console.log(b)
@@ -20,3 +26,8 @@ console.log(index)
 //console.log(slice)
 console.log(map)
 console.log(filter)
+console.log(include)
+//console.log(rev)
+//console.log(splice)
+console.table(flat)
+console.log(find)
