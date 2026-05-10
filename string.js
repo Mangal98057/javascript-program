37c8eeb5ffeb87910b8b50c609a7b11e4c3d620f
Mangal98057 <smangal621@gmail.com>
2026-05-10 07:26:13 +0530

    stringmethod

---
+++ string.js
@@ -0,0 +1,16 @@
+const newString=  "mangal singh"
+console.log(newString)
+
+console.log(newString.charAt(4))//search string 
+
+console.log(newString.trim())//remove extra space like front and end
+
+console.log(newString.toUpperCase())//all upper case charactor
+
+//console.log(newString.indexOf("singh")) //
+
+console.log(newString.replace("singh","2323"))// singh = text to find , 2323 =resplacement text 
+
+const newString2="seema"
+
+console.log(newString.concat(...newString2))// add to string
