963b1965dde80e6b2e39ec259b0b04f1f6744c42
Mangal98057 <smangal621@gmail.com>
2026-05-15 09:25:21 +0530

    function object and array

--- functionpm.js
+++ functionpm.js
@@ -7,3 +7,35 @@ function login (username){
 }
 console.log(login('mangal'))
 
+
+//chart function use add any product
+
+function addChartPrice(val1,val2,...num1){
+    return num1
+}
+console.log(addChartPrice(100,200,300,400)) 
+/*val1=100,
+val2=200,
+num1=[300,400]*/
+
+//array with function 
+
+const arr1=[200,300,400,500]
+function callArray(anyarray){
+      return anyarray[3]
+}
+console.log(callArray(arr1))
+
+
+//object with function
+const user={
+    userName:"mangal singh",
+    email:"mangal123@gmail.com"
+
+}
+function handleObject(anyobject){
+    console.log(`my user name ${user.userName} and my email ${user.email}`)
+
+
+}
+handleObject()
