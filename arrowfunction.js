dba35992a2d54b33bfa538b74a720d4d3dd916ec
Mangal98057 <smangal621@gmail.com>
2026-05-16 12:54:27 +0530

    this

--- arrowfunction.js
+++ arrowfunction.js
@@ -9,3 +9,16 @@ console.log(add(3,4))
 
 //greet function refrence 
 //greet() function execution 
+
+const obj ={
+    userName:"mangal singh",
+    email:"mangal123@gmail.com",
+    phone:4322323553,
+    welcome:function(){
+        console.log(`welcome my new function ,${this.userName}`)
+    }
+    
+}
+obj.welcome()
+obj.userName="sam"
+obj.welcome()
