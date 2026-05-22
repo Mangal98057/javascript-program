40e38c3c97abfc7f8e701c1036b8aae9cd1f7d17
Mangal98057 <smangal621@gmail.com>
2026-05-22 11:31:38 +0530

    truethyfalsy

---
+++ truethy.js
@@ -0,0 +1,37 @@
+
+
+
+const email="mangal123@main.com"
+if (email){
+    console.log("get user email")
+}else{
+    console.log("don't have email")
+}
+/*
+
+falsy value 
+
+
+| Falsy Value | Meaning         |
+| ----------- | --------------- |
+| `false`     | Boolean false   |
+| `0`         | Zero            |
+| `-0`        | Negative zero   |
+| `0n`        | BigInt zero     |
+| `""`        | Empty string    |
+| `null`      | Empty value     |
+| `undefined` | Undefined value |
+| `NaN`       | Not a Number    |
+
+
+truethly value
+
+true
+1
+-1
+"hello"
+"0"
+[]
+{}
+function(){}
+*/
