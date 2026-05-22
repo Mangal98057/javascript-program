799164ef449dc4089c42780c3c2deabdb31a45f2
Mangal98057 <smangal621@gmail.com>
2026-05-22 11:10:43 +0530

    switch case

---
+++ switch.js
@@ -0,0 +1,19 @@
+const day = 3
+switch (day) {
+    case 1:
+        console.log("monday")
+        
+        break;
+    case 2:
+       console.log("thusday") 
+        
+        break;
+    case 3:
+        console.log("wedesday")
+        
+        break;
+
+    default:
+        console.log("mistake")
+        break;
+}
