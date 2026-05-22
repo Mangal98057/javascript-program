67980fcc147a8986d95b780cea90c592226c5a5f
Mangal98057 <smangal621@gmail.com>
2026-05-22 11:14:47 +0530

    note

--- switch.js
+++ switch.js
@@ -17,3 +17,20 @@ switch (day) {
         console.log("mistake")
         break;
 }
+
+/*
+| Keyword   | Meaning                 |
+| --------- | ----------------------- |
+| `switch`  | Checks multiple values  |
+| `case`    | Defines condition       |
+| `break`   | Stops execution         |
+| `default` | Runs if no case matches |
+*/
+
+
+
+// The switch statement is used to check multiple possible values of a variable.
+
+// It helps execute different blocks of code based on different conditions.
+
+// switch is an alternative to using many if...else if statements.
