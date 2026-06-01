62cd9368147143b6d8dc8c24b047a20717f177af
Mangal98057 <smangal621@gmail.com>
2026-06-01 19:28:17 +0530

    reduce

--- reduce.js
+++ reduce.js
@@ -13,3 +13,50 @@ let initValue = 0;
 const sumWithinitValue=num.reduce((acc,curr)=>acc+curr)
 
 console.log(`Total Sum: ${sumWithinitValue}`)
+
+
+const books = [
+    {
+        name: "JavaScript Basics",
+        price: 200
+    },
+    {
+        name: "React Guide",
+        price: 350
+    },
+    {
+        name: "Node.js Mastery",
+        price: 400
+    },
+    {
+        name: "MongoDB Handbook",
+        price: 250
+    },
+    {
+        name: "HTML & CSS",
+        price: 150
+    },
+    {
+        name: "Python Essentials",
+        price: 300
+    },
+    {
+        name: "Data Structures",
+        price: 500
+    },
+    {
+        name: "Algorithms",
+        price: 450
+    },
+    {
+        name: "Web Development",
+        price: 600
+    },
+    {
+        name: "Full Stack MERN",
+        price: 700
+    }
+]
+
+const card=books.reduce((acc,items)=>acc+items.price,0)
+console.log(card)
